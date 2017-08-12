309e6b69b1b9d5b38ec66b9cea622852275d00be
vzapo <vrzaporojan@gmail.com>
2017-08-12 21:10:54 +0100

    server port changed

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-
 var bot = require('./bot.js');
+var port = process.env.PORT;
 
 app.get('/', (res,req) => {
   res.send('Hello world' + bot.bookN);
 });
 
-app.listen(8000);
+app.listen(port || 5000);
 console.log('Running');
