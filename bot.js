1d251f05e0e890c7143767d7043b693fbbfb6668
Zaporojan Victor <12383059+vzapo@users.noreply.github.com>
2017-09-21 11:40:04 +0100

    Change msg link creation
    Added link after msg length check

--- bot.js
+++ bot.js
@@ -11,28 +11,28 @@ var Twitter = new twit(config.config_twitter);
 // BOT =========================
 var wsirBot = function(alphabet){
     let date = new Date();
-    
+
     console.log(date.getHours() + ":" + date.getMinutes() + ' #WSIR Working... ');
-    
+
     // Get book
     getBook(alphabet, function(data){
         var year = date.getFullYear();
         var month;
-        if(date.getMonth >=10){ 
+        if(date.getMonth >=10){
             month = date.getMonth();
         }else{
             month = "0"+date.getMonth();
         }
         var day = date.getDate();
-        const time = year + "-"+month+ "-"+day;        
+        const time = year + "-"+month+ "-"+day;
         var book = data;
         var last_tweet_date;
-        
+
         // Compose params for twitter
         var msg = "Today's pick:"+ book.bookN +
         '-' + book.bookA +
-        ' @ ' + book.bookS;
-        
+        ' @ ';
+
         if(msg.length >= 140){
           const len = book.bookA.length / 2;
           let authorStr = book.bookA.substring(0, len);
@@ -46,28 +46,29 @@ var wsirBot = function(alphabet){
           }
         }
         
+        msg+= book.bookS;
         console.log(book);
 
         var tags = ' #WSIR #WhatShouldIRead #BOT';
-        
+
         // Set the params for the search
         var params = {
             q:'',
             status:msg + tags,
             lang: 'en',
         }
-        
+
         // Check if there is any previous post
         Twitter.get('statuses/user_timeline', {screen_name: 'wsirbot', count:1}, function(err, data, response) {
             if(err){console.log(err)}
             console.log(date.getHours() + ":" + date.getMinutes() + ' Checking for tweet in the last 24h...');
             var last_tweet_date = data[0].created_at.substring(8,11);
-            
+
             if(last_tweet_date == date.getDate()){
                 console.log('Found tweet in the last 24h,skip tweeting now!');
             }else{
                 console.log('No tweet found in the last 24h, tweeting now!');
-                
+
                 // Post the tweet
             	Twitter.post('statuses/update', params, function(err,data){
             	    var date = new Date();
@@ -87,7 +88,7 @@ var wsirBot = function(alphabet){
 
 // Launch the application
 wsirBot(letters);
-    
+
 // Repeat every 24 hour / 86400000
 var repeat_posting = setInterval(function(){
     wsirBot(letters);
