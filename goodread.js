59a8caef2e8b7027ccc483a649966d90cdd0ac9f
vzapo <vrzaporojan@gmail.com>
2017-09-23 21:49:05 +0100

    lower book recomandation to 3.8, good books are a bit lower of 4

--- goodread.js
+++ goodread.js
@@ -13,30 +13,30 @@ var searchBook = function(list,callback){
     var date = new Date();
     var nr = Math.floor(Math.random() * alphabet.length);
     var letter = alphabet[nr];
-    
+
     // Query for GoodRead search
     const query_book = {
         q:letter,
         page:'',
         type:'author'
     }
-    
+
     grclient.searchBooks(query_book).then(response => {
         var randomNrB = Math.floor(Math.random() * response.search.results.work.length);
-        
+
         do {
             randomNrB = Math.floor(Math.random() * response.search.results.work.length);
             console.log('Rating of book: '+response.search.results.work[randomNrB].average_rating);
         }
-        while (response.search.results.work[randomNrB].average_rating <= 4);
+        while (response.search.results.work[randomNrB].average_rating <= 3.8);
         var bookName = response.search.results.work[randomNrB].best_book.title;
         var bookAuthor = response.search.results.work[randomNrB].best_book.author.name;
         var bookId = response.search.results.work[randomNrB].best_book.id._;
-        
+
         book.bookA = bookAuthor;
         book.bookN = bookName;
         book.bookS = "https://www.goodreads.com/book/show/"+bookId;
-        
+
 	    console.log(date.getHours() + ":" + date.getMinutes() + ' Book picked: '+ bookAuthor + ' - ' + bookName);
         callback(book);
 
