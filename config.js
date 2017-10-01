9ecf8bc7bacf0347b1edf3cd0a5463b3389a0469
vzapo <vrzaporojan@gmail.com>
2017-10-01 19:10:33 +0100

    added file

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+
 //config.js
 /** Twitter app configuration
  * consumer_key
@@ -5,12 +6,12 @@
  * access_token
  * access_token_secret
  */
- 
+
 /** GoodRead configuration
- * Access Key  
+ * Access Key
  * Secret Key
  */ 
- 
+
 var config_goodreads = {
    key: 'k6sqCPBrgaRZ7JfCmJ5hgw',
 secret: 'NVmZqXZrAQiV8iXSTquPSLV0kDFS2CcRgRH9vKwJ4'
@@ -18,12 +19,12 @@ secret: 'NVmZqXZrAQiV8iXSTquPSLV0kDFS2CcRgRH9vKwJ4'
 }
 
 var config_twitter = {
-  consumer_key: '9mAGnTwivp5oeoiVesW1prEJX',  
+  consumer_key: '9mAGnTwivp5oeoiVesW1prEJX',
   consumer_secret: 'FZjzlsGD9XA13oeiu4FRHIbz5G3FnJ9Px9swQ7p9PMTheW7G8s',
-  access_token: '893195905482018818-Pl1U8ApGHtYZwWUAKcPJ8hCUyzpRHpn',  
+  access_token: '893195905482018818-Pl1U8ApGHtYZwWUAKcPJ8hCUyzpRHpn',
   access_token_secret: 'yuLCcLMCOZTymrEq4n1z6pqXa9v7fclAA5W2MwikM4RMr'
 }
- 
+
 module.exports = {
  config_twitter,
  config_goodreads
